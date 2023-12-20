c9f63a39d770f2e730f41174945830489f902dc3
Rakibul Hasan <87831331+rhakib@users.noreply.github.com>
2023-12-21 02:36:05 +0600

    file initialized and navbar added

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,41 @@
+import { Navbar } from "keep-react";
+import '../Components/Nav.css'
+
+const NavBar = () => {
+    return (
+        <div className="bg-teal-800">
+            <Navbar fluid={true} className="max-w-screen-xl mx-auto nav bg-teal-800">
+                <Navbar.Container className="flex items-center justify-between">
+                    <Navbar.Container className="flex items-center">
+                        <Navbar.Brand>
+                            <h2 className="font-bold text-white text-2xl">TaskForge</h2>
+                        </Navbar.Brand>
+                        <Navbar.Divider></Navbar.Divider>
+                        <Navbar.Container
+                            tag="ul"
+                            className="lg:flex hidden items-center justify-between gap-8"
+                        >
+                            <Navbar.Link linkName="Home" />
+                            <Navbar.Link linkName="Dashboard" />
+                            <Navbar.Link linkName="Contact us" />
+                        </Navbar.Container>
+
+                        <Navbar.Collapse collapseType="sidebar">
+                            <Navbar.Container tag="ul" className="flex sideBar flex-col gap-5">
+                                <Navbar.Link linkName="Home" />
+                                <Navbar.Link linkName="Dashboard" />
+                                <Navbar.Link linkName="Contact us" />
+                            </Navbar.Container>
+                        </Navbar.Collapse>
+                    </Navbar.Container>
+
+                    <Navbar.Container className="flex gap-2">
+                        <Navbar.Toggle />
+                    </Navbar.Container>
+                </Navbar.Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default NavBar;
---
+++ src/Layout/Root.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from '../Components/NavBar';
+import { Outlet } from 'react-router-dom';
+
+const Root = () => {
+    return (
+        <div>
+            <NavBar></NavBar>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default Root;
